95ea7f0cb9fbdc6e3ec0ad969443d3d2fe71fa28
pinakisaha86 <pinaki.saha86@gmail.com>
2024-06-01 09:58:40 +0530

    updated daily scripts

--- spark_script/daily_load.py
+++ spark_script/daily_load.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
 
     print("\nCreating dataframe ingestion CSV file using 'SparkSession.read.format()'")
 
-    df2 = spark.read.option("header", "true").option("delimiter", ",").csv("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/landing/daily/transaction.csv")
+    df2 = spark.read.option("header", "true").option("delimiter", ",").csv("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/landing/daily/")
     df2.show(20, False)
 
 
